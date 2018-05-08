ef2f7812fe902324db6c92b87fe4cfc044ee6d05
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 22:04:01 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
       info.each do |sub_cat, deet|
         deet.each do |name|
             pigeon_names[name] = {}
-            # pigeon_names[name][category] = []
+            pigeon_names[name][category] = []
             # pigeon_names[name][category] << sub_cat.to_s
             binding.pry
         end
