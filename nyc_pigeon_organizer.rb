71ea66f2bb70a7ac6f848268426e20c6bd0bddec
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 20:20:22 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,33 @@
 require "pry"
 def nyc_pigeon_organizer(data)
 
-  bag_of_names = []
+  bag_of_names = {}
 
   data.each do |category, info|
     info.each do |color, name|
-      bag_of_names << name
+
+      unless bag_of_names[name]
+        
+      end
+
     end
   end
-  solo_birds = bag_of_names.flatten.uniq
-  binding.pry
+
+
 end
+
+
+
+
+# def nyc_pigeon_organizer(data)
+#
+#   bag_of_names = []
+#
+#   data.each do |category, info|
+#     info.each do |color, name|
+#       bag_of_names << name
+#     end
+#   end
+#   solo_birds = bag_of_names.flatten.uniq
+#   binding.pry
+# end
