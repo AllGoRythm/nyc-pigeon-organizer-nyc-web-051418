a26955122b89d934c6192c4e06431ca248ea6553
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 20:02:21 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,17 @@
 require "pry"
 def nyc_pigeon_organizer(data)
 
+  all_names = []
+
   data.each do |category, info|
 
-    info.each do |k, v|
-      binding.pry
+    info.each do |colors, names|
+
+      if names.is_a?(String)
+        all_names << names
+        binding.pry
+      end
+
     end
 
   end
