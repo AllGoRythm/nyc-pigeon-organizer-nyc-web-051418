3583351582395132d69bc6491a0b696a9945d9ae
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 21:43:14 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,3 +17,23 @@ def nyc_pigeon_organizer(data)
 end
 
 
+
+#     "Theo" => {
+#   :color => ["purple", "grey"],
+#   :gender => ["male"],
+#   :lives => ["Subway"]
+# }
+
+
+# def nyc_pigeon_organizer(data)
+#
+#   bag_of_names = []
+#
+#   data.each do |category, info|
+#     info.each do |color, name|
+#       bag_of_names << name
+#     end
+#   end
+#   solo_birds = bag_of_names.flatten.uniq
+#   binding.pry
+# end
