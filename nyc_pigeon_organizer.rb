0997cbe6e7a5ab8d9e67330e0b071d049ca87670
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 20:10:35 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,12 +5,12 @@ def nyc_pigeon_organizer(data)
   solo_birds = bag_of_names.flatten.uniq
 
   data.each do |category, info|
-    binding.pry
-    # info.each do |colors, names|
-    #
-    #   binding.pry
-    #
-    # end
+    
+    info.each do |colors, names|
+
+      binding.pry
+
+    end
 
   end
 
